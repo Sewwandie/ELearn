ef9eea2569c029c01d63211971c92586d67f0ed8
Prageeth <prageethda@gmail.com>
2019-10-31 19:46:10 +0530

    commit 8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {HomeComponent} from './home'
-import {RegistrationComponent} from './registration'
+import {HomeComponent} from './home';
+import {RegistrationComponent} from './registration';
+import { DashboardComponent} from './dashboard';
+import{AdminPortalComponent} from './admin-portal';
 
 
 const routes: Routes =[
   { path: '', redirectTo: 'index', pathMatch: 'full' },
   { path: 'index',                component: HomeComponent },
-  {path:'Register', component:RegistrationComponent}
+  {path:'Register', component:RegistrationComponent},
+  {path:'dash',component:DashboardComponent},
+  {path:'Adash',component:AdminPortalComponent}
 ];
 
 @NgModule({
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService,UserService} from '../services';
+import { Router,  } from '@angular/router';
+import { User } from '../models';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  stds:any[];
+  corses:any[];
+  quizes:any[];
+  currentUser:User;
+
+  constructor(private authenticationService :AuthenticationService,
+    private router:Router,
+    private userService:UserService
+    ) {
+      this.currentUser = this.authenticationService.currentUserValue;
+     }
 
   ngOnInit() {
+    this.loadstds();
+    this.loadquizs();
+    this.loadcorses();
+    
   }
+
+  btnClick= function () {
+    this.authenticationService.logout();
+    this.router.navigateByUrl('index');
+};
+
+
+
+
+private loadstds() {
+  this.userService.getAllstds()
+        .subscribe(data => this.stds = data);
+  
+}
+
+private loadquizs() {
+  this.userService.getAllQuizs()
+        .subscribe(data => this.quizes = data);
+  
+}
+private loadcorses() {
+  this.userService.getAllcorses()
+        .subscribe(data => this.corses = data);
+  
+}
 
 }
